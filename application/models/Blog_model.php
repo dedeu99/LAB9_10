b5346f13799e19639a363f02490d371742cd4c9a
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-20 16:53:58 +0000

    asd

---
+++ application/models/Blog_model.php
@@ -0,0 +1,14 @@
+<?php
+class Blog_model extends CI_Model {
+	public function __construct()
+	{
+		$this->load->database();
+	}
+	public function get_posts()
+	{
+		$query_RAW = "SELECT * FROM microposts ORDER BY created_at DESC";
+		$query = $this->db->query($query_RAW);
+		return $query->result_array();	
+	}
+}
+?>
