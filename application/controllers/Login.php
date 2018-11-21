47aac53e75720edc52c1593b187da15bacfa6034
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-21 00:11:02 +0000

    asd

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -22,5 +22,23 @@
 		   
 		    $this->smarty->view('application/views/templates/index_template.tpl', $data);		
 		}
+
+
+		public function login()
+		{
+			$data['hidden'] = 'hidden';
+			$data['hidden2'] = '';
+			$data['USERNAME'] = '';	
+			$data['USER_ID'] = '';
+			
+		    
+		    $data['blogs'] = NULL;
+
+		    /*$data['link1']=site_url("blog/index");
+		    $data['link2']=site_url()."/blog/login";
+		    $data['link3']=site_url()."/blog/register";*/
+		   
+		    $this->smarty->view('application/views/templates/index_template.tpl', $data);		
+		}
 	}
 ?> 
