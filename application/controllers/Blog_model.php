51e19ce3fa76dde5b284515c9bb2123270b0c70c
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-26 16:59:47 +0000

    asd

--- application/controllers/Blog_model.php
+++ application/controllers/Blog_model.php
@@ -10,7 +10,7 @@ class Blog_model extends CI_Model {
 		$query = $this->db->query($query_RAW);
 		return $query->result_array();	
 	}
-	public function registerUser(name,email,password)
+	public function registerUser($name,$email,$password)
 	{
 		echo $name;
 		/*$query_RAW = INSERT INTO users (name,email,created_at,updated_at,password_digest,remember_digest,admin) VALUES ('$name','$email',NOW(),NOW(),'$password',NULL,0);
