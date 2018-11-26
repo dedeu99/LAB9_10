dfaf9dcc4f61e6690ca217c6e1a1b74b8760df58
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-26 16:43:11 +0000

    asd

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -31,7 +31,7 @@
 
             $this->load->library('form_validation');
 
-            $this->form_validation->set_rules('name', 'Username', 'required|min_length[5]|max_length[12]|is_unique[users.username]', array('required' => 'You must provide a %s.' ,'is_unique'     => 'This %s already exists.'));
+            $this->form_validation->set_rules('name', 'Username', 'required|min_length[5]|max_length[12]|is_unique[users.name]', array('required' => 'You must provide a %s.' ,'is_unique'     => 'This %s already exists.'));
 
             $this->form_validation->set_rules('password', 'Password', 'required|min_length[7]', array('required' => 'You must provide a %s.'));
 			$this->form_validation->set_rules('passwordConfirmation', 'Password Confirmation', 'required|matches[password]', array('required' => 'You must provide a %s.'));
