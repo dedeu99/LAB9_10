fdb9149d4fc724585302c2bc5a1c9f3b56935667
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-26 17:22:27 +0000

    asd

--- application/controllers/Blog_model.php
+++ application/controllers/Blog_model.php
@@ -12,13 +12,10 @@ class Blog_model extends CI_Model {
 	}
 	public function register_user()
 	{
-		/*$name,$email,$password*/
-		//echo $name;
+		
 		echo "HELLO";
-		/*$query_RAW = INSERT INTO users (name,email,created_at,updated_at,password_digest,remember_digest,admin) VALUES ('$name','$email',NOW(),NOW(),'$password',NULL,0);
-		$query = $this->db->query($query_RAW);
-		return $query->result_array();*/
-		return 0;
+		
+		return "asd";
 	}
 }
 ?>
