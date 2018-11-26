8c9e7698b8fbee63cd760736442c70068d2bcad8
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-26 17:17:52 +0000

    asd

--- application/controllers/Blog_model.php
+++ application/controllers/Blog_model.php
@@ -18,7 +18,7 @@ class Blog_model extends CI_Model {
 		/*$query_RAW = INSERT INTO users (name,email,created_at,updated_at,password_digest,remember_digest,admin) VALUES ('$name','$email',NOW(),NOW(),'$password',NULL,0);
 		$query = $this->db->query($query_RAW);
 		return $query->result_array();*/
-		return ;
+		return 0;
 	}
 }
 ?>
