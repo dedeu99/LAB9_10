ae253907f6572ebf2f468dd78be2036e4810fb76
dedeu99 <andre.calixto.martins@gmail.com>
2018-11-29 01:28:20 +0000

    asd

--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -8,7 +8,7 @@ class Blog_model extends CI_Model {
 	{
 		$query_RAW = //"SELECT * FROM microposts ORDER BY created_at DESC";
 
-		"SELECT microposts.id, content, user_id,microposts.created_at,microposts.updated_at,name FROM microposts join users on user_id==users.id ORDER BY created_at DESC";
+		"SELECT microposts.id, content, user_id,microposts.created_at,microposts.updated_at,name FROM microposts join users on user_id=users.id ORDER BY created_at DESC";
 
 
 
