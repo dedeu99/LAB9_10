84e0e822dd01ea16305a564a6f92c24f9088fe41
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-03 18:14:45 +0000

    watch me

--- application/models/Blog_model.php
+++ application/models/Blog_model.php
@@ -111,7 +111,7 @@ VALUES (value1, value2, value3, ...);*/
 	}
 	public function get_replies($id)
 	{
-		$sql="SELECT replies.id,content,user_id,micropost_id,replies.created_at,name FROM replies JOIN users ON users.user_id=replies.user_id WHERE replies.micropost_id=$id ORDER BY replies.created_at DESC";
+		$sql="SELECT replies.id,content,user_id,micropost_id,replies.created_at,name FROM replies JOIN users ON users.id=user_id WHERE replies.micropost_id=$id ORDER BY replies.created_at DESC";
 		$query = $this->db->query($sql);
 		return $query->result_array();
 	}
