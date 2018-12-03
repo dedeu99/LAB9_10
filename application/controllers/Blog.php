f0c4ea70260244ceacb1d65dab3a042b01e5eaab
dedeu99 <andre.calixto.martins@gmail.com>
2018-12-03 17:31:14 +0000

    watch me

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,14 +1,14 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed'); 
+	error_reporting(E_ALL);
+			ini_set('display_errors', 1);
 
 	class Blog extends CI_Controller {
 		public function __construct()
 		{
 			parent::__construct();
 			
-			error_reporting(E_ALL);
-			ini_set('display_errors', 1);
-
+			
 
 			$this->load->model('blog_model');
 
